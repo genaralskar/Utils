52e8a3c8eb880523e64f5e3a095fc40a14a996a6
Collin <genaralskar1@gmail.com>
2020-03-19 14:23:40 -0600

    add DrawEventLine

---
+++ DrawEventLines/DrawEventLines.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace genaralskar
+{
+    public static class DrawEventLines
+    {
+        public static void Draw(UnityEvent e, Transform origin)
+        {
+            int c = e.GetPersistentEventCount();
+            Vector3 location = origin.position;
+            for (int i = 0; i < c; i++)
+            {
+                var obj = e.GetPersistentTarget(i);
+                if (!obj) continue;
+                
+                MonoBehaviour mb = obj as MonoBehaviour;
+                if (mb == null)
+                {
+                    //If the object is not a MonoBehaviour, try to find its object by name
+                    GameObject go = GameObject.Find(obj.name);
+                    if (go)
+                    {
+                        Gizmos.color = Color.red;
+                        location = go.transform.position;
+                    }
+                }
+                else
+                {
+                    Gizmos.color = Color.green;
+                    location = mb.transform.position;
+                }
+            
+                Gizmos.DrawLine(origin.position, location);
+            }
+        }
+    }
+}
---
+++ DrawEventLines/README.md
@@ -0,0 +1,17 @@
+This script will draw lines in the editor to any objects connected with UnityEvents. The script should be placed somewhere in your Assets, and can then be accessed from your scripts. It must be called in either OnDrawGizmos or OnDrawGizmosSelected
+```C#
+public UnityEvent myEvent;
+
+private void OnDrawGizmosSelected()
+    {
+        genaralskar.DrawEventLines.Draw(myEvent, transform);
+    }
+```
+
+`myEvent` is your UnityEvent
+`transform` is the transform where you want the lines to connect
+
+The line will be green if the event is calling a MonoBehavior (usually custom script), or red otherwise (usually a built in componenet). These colors can be changed in the script.
+This is done because if the event is not calling a MonoBehavior it must find the GameObject by name, so if two GameObjects in your project have the same name it may not connect properly.
+
+This does not currently support UnityEvents that require information like `UnityEvent<int> myEvent`
