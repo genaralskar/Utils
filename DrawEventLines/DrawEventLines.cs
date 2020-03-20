72c747bd30974a81a8b6956c1ec2bd1c15a1d64c
Collin <genaralskar1@gmail.com>
2020-03-19 22:25:26 -0600

    update DrawEventLines

--- DrawEventLines/DrawEventLines.cs
+++ DrawEventLines/DrawEventLines.cs
@@ -6,31 +6,29 @@ namespace genaralskar
     public static class DrawEventLines
     {
         public static void Draw(UnityEvent e, Transform origin)
-        {
+        {            
             int c = e.GetPersistentEventCount();
             Vector3 location = origin.position;
             for (int i = 0; i < c; i++)
             {
                 var obj = e.GetPersistentTarget(i);
-                if (!obj) continue;
+                if (!obj) continue; //In the case of an unassigned UnityEvent, skip it
                 
-                MonoBehaviour mb = obj as MonoBehaviour;
-                if (mb == null)
+                var mb = obj as Component;
+                if (mb != null)
                 {
-                    //If the object is not a MonoBehaviour, try to find its object by name
-                    GameObject go = GameObject.Find(obj.name);
-                    if (go)
-                    {
-                        Gizmos.color = Color.red;
-                        location = go.transform.position;
-                    }
+                    location = mb.transform.position;
                 }
                 else
                 {
-                    Gizmos.color = Color.green;
-                    location = mb.transform.position;
+                    //If the object is not a Component, get it as a GameObject
+                    var go = obj as GameObject;
+                    if (go)
+                    {
+                        location = go.transform.position;
+                    }
                 }
-            
+
                 Gizmos.DrawLine(origin.position, location);
             }
         }
--- DrawEventLines/README.md
+++ DrawEventLines/README.md
@@ -1,6 +1,6 @@
 This script will draw lines in the editor to any objects connected with UnityEvents. The script should be placed somewhere in your Assets, and can then be accessed from your scripts. It must be called in either OnDrawGizmos or OnDrawGizmosSelected
 
-In action: https://gfycat.com/slipperyheftygopher
+In action: https://gfycat.com/competentfocusedgelada
 
 Implementation
 ```C#
@@ -8,6 +8,7 @@ public UnityEvent myEvent;
 
 private void OnDrawGizmosSelected()
 {
+	Gizmos.Color = Color.green;
 	genaralskar.DrawEventLines.Draw(myEvent, transform);
 }
 ```
@@ -15,7 +16,6 @@ private void OnDrawGizmosSelected()
 `myEvent` is your UnityEvent
 `transform` is the transform where you want the lines to connect
 
-The line will be green if the event is calling a MonoBehavior (usually custom script), or red otherwise (usually a built in componenet). These colors can be changed in the script.
-This is done because if the event is not calling a MonoBehavior it must find the GameObject by name, so if two GameObjects in your project have the same name it may not connect properly.
+
 
 This does not currently support UnityEvents that require information like `UnityEvent<int> myEvent`
